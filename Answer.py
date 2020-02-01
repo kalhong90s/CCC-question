2c136af223b7505dcbe84d8538a3c6c04206a52c
kalhong90s <60230725+kalhong90s@users.noreply.github.com>
2020-02-01 18:33:14 +0700

    Add files via upload

---
+++ Answer.py
@@ -0,0 +1,87 @@
+
+'''
+1. Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+'''
+ans = []
+for num in range(1,100+1):
+    if (num % 3 == 0 and num % 5 == 0):
+        ans.append("FizzBuzz")
+    elif num % 3 == 0:
+        ans.append("Fizz")
+    elif num % 5 == 0:
+        ans.append("Buzz")
+    else:
+        ans.append(str(num))
+print(ans)
+
+'''
+2. Write a program that determine whether or not an integer input is a leap year.
+'''
+year = int(input("Please Enter the Year : "))
+
+if (year%400 == 0):
+    print(year,"-> true")
+elif (year%100 == 0):
+    print(year,"-> false")
+elif (year%4 == 0):
+    print(year,"-> true")
+else:
+    print(year,"-> false")
+'''
+3. Write a program that produce the following output giving an integer input n.
+
+'''
+# 3.1
+n = int(input("Please Enter the number : "))
+for i in range(0, n):
+    for j in range(0, i+1):
+        print("*",end="")
+    print()
+    
+    
+#3.2
+n = int(input("Please Enter the number :"))
+for i in range(0,n):
+    for j in range(n - i):
+        print (" ",end="")
+    for j in range(0,i + 1):
+        print("*",end="")
+    print()
+
+#3.3
+n = int(input("Please Enter the number :"))
+for i in range(n):
+    print(" "*(n-i-1)+"* ",end="")
+    if i>=1:
+        print(" "*(2*i-1)+"* ",end="")
+    print()
+
+#3.4
+n = int(input("Please Enter the number :"))       
+for i in range(0,n):                       
+    for j in range(0,n):
+        if(i==j or j==n-1-i):
+            print("*",end=" ")             
+        else:
+            print(" ",end=" ")             
+    print()                                
+
+#3.5 not work of even number
+n = int(input("Please Enter the number :"))     
+for i in range(1, n+1):
+    i = i - (n//2 +1)
+    if i < 0:
+        i = -i
+    print(" " * i + "*" * (n - i*2) + " "*i)
+  
+#4.0 
+n = int(input("Please Enter the number :")) 
+
+for num in range(1, n + 1):  
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+ 
